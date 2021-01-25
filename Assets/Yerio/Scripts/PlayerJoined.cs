3dae4340612726b9089c366245968b4a0aff1d2c
frzDazzT <58939437+frzDazzT@users.noreply.github.com>
2021-01-25 18:14:55 +0100

    Update PlayerJoined.cs

--- Assets/Yerio/Scripts/PlayerJoined.cs
+++ Assets/Yerio/Scripts/PlayerJoined.cs
@@ -2,21 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Bolt;
+using Bolt.Matchmaking;
 
 public class PlayerJoined : Bolt.EntityBehaviour<IPlayerControllerState>
 {
     [SerializeField] Camera playerCamera;
-
-    public override void Detached()
-    {
-        base.Detached();      
-    }
 
     private void Update()
     {
         if(entity.IsOwner && !playerCamera.gameObject.activeInHierarchy)
         {
             playerCamera.gameObject.SetActive(true);
+        }
+
+        if (!BoltNetwork.IsConnected && entity.IsAttached)
+        {
+            SceneManager.LoadScene(0);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = false;
         }
     }
 }
