5d2895a012ae34cb44e06d22c1de648795882f7e
frzDazzT <58939437+frzDazzT@users.noreply.github.com>
2021-01-26 16:34:03 +0100

    Update PlayerLeave.cs

--- Assets/Yerio/Scripts/PlayerLeave.cs
+++ Assets/Yerio/Scripts/PlayerLeave.cs
@@ -1,16 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using Bolt;
 
-[BoltGlobalBehaviour(BoltNetworkModes.Client)]
 public class PlayerLeave : GlobalEventListener
 {
     public override void Disconnected(BoltConnection connection)
     {
         base.Disconnected(connection);
         connection.Disconnect();
-        BoltNetwork.LoadScene("MainMenu");
+        
     }
 }
