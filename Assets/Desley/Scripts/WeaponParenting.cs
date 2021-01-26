d6a3dc928de4a52b777f6cf95b02909c1447e98d
Desleyo <Desleyoord@gmail.com>
2021-01-26 15:21:14 +0100

    weapon parenting

---
+++ Assets/Desley/Scripts/WeaponParenting.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponParenting : MonoBehaviour
+{
+    [SerializeField] Transform cam;
+
+    void Update()
+    {
+        if(cam.gameObject.activeSelf == true)
+        {
+            transform.SetParent(cam);
+        }
+    }
+}
