b21d6253f664f71a5c06bf0bf788663badb277c7
Desleyo <Desleyoord@gmail.com>
2021-01-26 15:26:53 +0100

    push

--- Assets/Desley/Scripts/WeaponRotation.cs
+++ Assets/Desley/Scripts/WeaponRotation.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeaponParenting : MonoBehaviour
+public class WeaponRotation : MonoBehaviour
 {
     [SerializeField] Transform cam;
 
