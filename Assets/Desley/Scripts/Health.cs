d8be73f8706515c7f8e700d239d828e39697acb7
Desleyo <Desleyoord@gmail.com>
2021-01-27 15:43:47 +0100

    some killing

---
+++ Assets/Desley/Scripts/Health.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : Bolt.EntityBehaviour<IPlayerControllerState>
+{
+    [SerializeField] int currentHealth, maxHealth;
+
+    public override void Attached()
+    {
+        base.Attached();
+    }
+
+    public override void SimulateOwner()
+    {
+        if(currentHealth <= 0)
+        {
+            Debug.LogWarning("sack");
+            BoltNetwork.Destroy(gameObject);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+    }
+}
---
+++ Assets/Desley/Scripts/Shoot.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : Bolt.EntityBehaviour<IPlayerControllerState>
+{
+    [SerializeField] Camera cam;
+    [SerializeField] GameObject flash;
+    [SerializeField] Transform muzzle;
+    [SerializeField] int damage, range;
+    [SerializeField] float flashTimer = Mathf.Infinity;
+
+    public override void Attached()
+    {
+        base.Attached();
+        state.OnWeaponTrigger = ShootRaycast;
+    }
+
+    public override void SimulateOwner()
+    {
+        base.SimulateOwner();
+        if (Input.GetButtonDown("Fire1") && entity.IsOwner)
+        {
+            state.WeaponTrigger();
+            flash.SetActive(true);
+            flashTimer = .1f;
+        }
+
+        flashTimer -= Time.deltaTime;
+
+        if(flashTimer <= 0)
+        {
+            flash.SetActive(false);
+            flashTimer = Mathf.Infinity;
+        }
+    }
+
+    public void ShootRaycast()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit))
+        {
+            Health health = hit.collider.gameObject.GetComponent<Health>();
+            if(health != null)
+            {
+                health.TakeDamage(damage);
+            }
+        }
+    }
+}
