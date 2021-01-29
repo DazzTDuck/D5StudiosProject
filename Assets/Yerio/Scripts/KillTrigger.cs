04e110fe7ed033434abc194e3cc5f5488168dd66
frzDazzT <58939437+frzDazzT@users.noreply.github.com>
2021-01-29 18:13:02 +0100

    Update KillTrigger.cs

--- Assets/Yerio/Scripts/KillTrigger.cs
+++ Assets/Yerio/Scripts/KillTrigger.cs
@@ -23,7 +23,7 @@ public class KillTrigger : MonoBehaviour
 
     IEnumerator ResetRequest()
     {
-        yield return new WaitForSeconds(0.25f);
+        yield return new WaitForSeconds(1f);
 
         requestSent = false;
     }
