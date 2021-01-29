68d6fc122fb2a6129984f0bd7837a2204734e8d2
frzDazzT <58939437+frzDazzT@users.noreply.github.com>
2021-01-29 18:08:55 +0100

    Update KillTrigger.cs

--- Assets/Yerio/Scripts/KillTrigger.cs
+++ Assets/Yerio/Scripts/KillTrigger.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class KillTrigger : MonoBehaviour
 {
+    bool requestSent = false;
+
     private void OnTriggerEnter(Collider other)
     {
         var health = other.gameObject.GetComponent<Health>();
-        if (health)
+        if (health && !requestSent)
         {
             //Create DamageRequest, set entity to ent and Damage to damage, then send
             var request = DamageRequest.Create();
             request.Entity = health.GetComponentInParent<BoltEntity>();
             request.Damage = 100;
             request.Send();
+            requestSent = true;
+            StartCoroutine(ResetRequest());
         }
+    }
+
+    IEnumerator ResetRequest()
+    {
+        yield return new WaitForSeconds(0.25f);
+
+        requestSent = false;
     }
 }
