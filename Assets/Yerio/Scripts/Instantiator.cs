7b8b89ef6a3c1364c640951b2edf923903aacb7b
frzDazzT <58939437+frzDazzT@users.noreply.github.com>
2021-02-18 12:23:16 +0100

    placeholder water

---
+++ Assets/Yerio/Scripts/Instantiator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bolt;
+
+public class Instantiator : MonoBehaviour
+{
+    [SerializeField] GameObject[] objectsToInstatiateOnNetwork;
+    
+    private void Start()
+    {
+        foreach (var gameObject in objectsToInstatiateOnNetwork)
+        {
+            BoltNetwork.Instantiate(gameObject);
+        }
+    }
+}
