6ce6d7b6a37e45f89cd89b0e82e81544b1fb5be9
frzDazzT <58939437+frzDazzT@users.noreply.github.com>
2021-02-22 12:25:32 +0100

    kill leaderboard UI & update script

---
+++ Assets/Yerio/Scripts/UpdateKillCounter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UpdateKillCounter : MonoBehaviour
+{
+    [SerializeField] TMP_Text team1Counter;
+    [SerializeField] TMP_Text team2Counter;
+
+    GameInfo gameInfo;
+
+    private void Update()
+    {
+        if (!gameInfo) { gameInfo = FindObjectOfType<GameInfo>(); }
+
+        if (gameInfo)
+        {
+            team1Counter.text = gameInfo.state.Team1Kills.ToString();
+            team2Counter.text = gameInfo.state.Team2Kills.ToString();
+        }
+    }
+}
