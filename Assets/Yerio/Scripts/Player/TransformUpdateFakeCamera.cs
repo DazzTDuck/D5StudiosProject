bfdfd8dfac0eebbf49856cfef5c79473ab70e32b
frzDazzT <58939437+frzDazzT@users.noreply.github.com>
2021-03-01 16:42:12 +0100

    camera change

---
+++ Assets/Yerio/Scripts/Player/TransformUpdateFakeCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformUpdateFakeCamera : Bolt.EntityBehaviour<IPlayerControllerState>
+{
+    public override void Attached()
+    {
+        state.SetTransforms(state.GunTransform, transform);
+    }
+
+}
+
