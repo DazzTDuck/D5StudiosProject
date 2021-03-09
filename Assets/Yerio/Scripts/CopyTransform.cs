2afd38f9b9735b8b1c22e388ca36c08a64c005be
frzDazzT <58939437+frzDazzT@users.noreply.github.com>
2021-03-09 16:00:26 +0100

    not still work

---
+++ Assets/Yerio/Scripts/CopyTransform.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyTransform : MonoBehaviour
+{
+    [SerializeField] Transform toCopy;
+
+    private void Update()
+    {
+        transform.position = toCopy.position;
+        transform.rotation = toCopy.rotation;
+    }
+}
