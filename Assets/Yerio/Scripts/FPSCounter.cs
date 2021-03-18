7009fc2fb4a20b716c15a78fe4d1fb666e247248
frzDazzT <58939437+frzDazzT@users.noreply.github.com>
2021-03-18 12:31:55 +0100

    fps counter

---
+++ Assets/Yerio/Scripts/FPSCounter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FPSCounter : MonoBehaviour
+{
+    TMP_Text fpsText;
+    float _hudRefreshRate = .25f;
+
+    private float timer;
+
+    private void Awake()
+    {
+        fpsText = GetComponent<TMP_Text>();
+    }
+
+    private void Update()
+    {
+        if (Time.unscaledTime > timer)
+        {
+            int fps = (int)(1f / Time.unscaledDeltaTime);
+            fpsText.text = "FPS: " + fps;
+            timer = Time.unscaledTime + _hudRefreshRate;
+        }
+    }
+}
